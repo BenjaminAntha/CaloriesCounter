fa15d1770d2d8af8ca6b795ecebf6719eef51e61
Benjamin Anthamatten <benjamin.antha.2001@gmail.com>
2022-11-21 16:30:23 +0100

    Initial Commit

---
+++ CaloriesCounter/CaloriesCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesCounterApp.swift
+//  CaloriesCounter
+//
+//  Created by Benjamin Anthamatten on 21.11.22.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
